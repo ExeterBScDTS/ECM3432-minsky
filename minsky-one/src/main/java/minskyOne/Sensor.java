ff2199860c1bb95851180778b3156bdb9811efec
Michael Saunby <mike@saunby.net>
2018-12-12 20:00:23 +0000

    mock classes

---
+++ minsky-one/src/main/java/minskyOne/Sensor.java
@@ -0,0 +1,8 @@
+package minskyOne;
+
+public class Sensor{
+
+    public String htmlTr(){
+        return "<tr><td>hello</td><td>world</td></tr>";
+    }
+}
---
+++ minsky-one/src/main/java/minskyOne/Status.java
@@ -0,0 +1,14 @@
+package minskyOne;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Status{
+
+    public List<Sensor> getSensors() {
+        List<Sensor> sensors = new ArrayList<Sensor>();
+
+        return sensors;
+    }
+
+}
