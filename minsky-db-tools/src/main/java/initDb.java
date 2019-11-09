129ab7a0a2cb69c6f1a1cfdd67bb970c61e7b5bf
Michael Saunby <mike@saunby.net>
2019-11-09 23:45:44 +0000

    .

--- minsky-db-tools/src/main/java/initDb.java
+++ minsky-db-tools/src/main/java/initDb.java
@@ -34,10 +34,21 @@ public class initDb {
         String url = "jdbc:sqlite:/tmp/test.db";
         
         // SQL statement for creating a new table
-        String sql = "CREATE TABLE IF NOT EXISTS warehouses (\n"
-                + "    id integer PRIMARY KEY,\n"
-                + "    name text NOT NULL,\n"
-                + "    capacity real\n"
+ 
+        String sql = "create table users  (\n"
+                + "     id integer primary key,\n"
+                + "     username varchar(100) not null unique,\n"
+                + "     pwd varchar(20) not null\n"
+                + ");"
+                + "create table roles  (\n"
+                + "     id integer primary key,\n"
+                + "     role varchar(100) not null unique key\n"
+                + " );"
+                + "create table user_roles  (\n"
+                + "     user_id integer not null,\n"
+                + "     role_id integer not null,\n"
+                + "     unique key (user_id, role_id),\n"
+                + "    index(user_id)\n"
                 + ");";
         
         try (Connection conn = DriverManager.getConnection(url);
