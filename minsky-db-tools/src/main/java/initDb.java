c0dde759d7f423077525b9d74e90cf92a84237c6
Michael Saunby <mike@saunby.net>
2019-11-09 23:19:05 +0000

    .

--- minsky-db-tools/src/main/java/initDb.java
+++ minsky-db-tools/src/main/java/initDb.java
@@ -3,6 +3,7 @@ import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class initDb {
 
@@ -27,10 +28,32 @@ public class initDb {
         }
     }
  
+
+    public static void createNewTable() {
+        // SQLite connection string
+        String url = "jdbc:sqlite:/tmp/test.db";
+        
+        // SQL statement for creating a new table
+        String sql = "CREATE TABLE IF NOT EXISTS warehouses (\n"
+                + "    id integer PRIMARY KEY,\n"
+                + "    name text NOT NULL,\n"
+                + "    capacity real\n"
+                + ");";
+        
+        try (Connection conn = DriverManager.getConnection(url);
+                Statement stmt = conn.createStatement()) {
+            // create a new table
+            stmt.execute(sql);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        createNewDatabase("test.db");
+        //createNewDatabase("test.db");
+        createNewTable();
     }
 }
