e60938e879a2208c92b15c5a11bad69a4a0717f9
Michael Saunby <mike@saunby.net>
2019-12-15 14:38:20 +0000

    .

--- minsky-front-end/src/tircanvas-TIRCanvas.spec.ts
+++ minsky-front-end/src/tircanvas-TIRCanvas.spec.ts
@@ -1,5 +1,4 @@
-// See https://journal.artfuldev.com/unit-testing-node-applications-with-typescript-using-mocha-and-chai-384ef05f32b2
-
+// Unit-tests for tircanvas.ts TIRCanvas class.
 import {Palette, TIRCanvas} from './tircanvas';
 import { JSDOM } from 'jsdom';
 import { expect } from 'chai';
@@ -10,28 +9,9 @@ const dom = new JSDOM('<!doctype html><html><body><div><canvas id="canvas" width
 let p = new Palette();
 p.setLength(100);
 
-describe('Palette.getColour()', () => {
-
-  it('0.0 should return [0,0,0]', () => {
-    const result = p.getColour(0);
-    expect(result).to.equal('rgb(0,0,0)');
-  });
-
-  it('1.0 should return [255,255,255]', () => {
-    const result = p.getColour(1);
-    expect(result).to.equal('rgb(255,255,255)');
-  });
-
-});
-
 let c = <HTMLCanvasElement> dom.window.document.getElementById('canvas');
 let tircanv = new TIRCanvas(c,p);
 
-//console.log(tircanv.palIdx(10));
-//console.log(tircanv.palIdx(15));
-//console.log(tircanv.palIdx(20));
-//console.log(tircanv.palIdx(25));
-//console.log(tircanv.palIdx(30));
 
 describe('TIRCanvas.palIdx()', () => {
 
