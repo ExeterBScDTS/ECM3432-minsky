46f06e7575c5836b85bdf303c00ee882cf68a6b8
Michael Saunby <mike@saunby.net>
2019-12-17 23:29:52 +0000

    .

--- minsky-one/src/main/java/minskyone/Utils.java
+++ minsky-one/src/main/java/minskyone/Utils.java
@@ -5,24 +5,25 @@ import javax.servlet.http.HttpServletRequest;
 
 public class Utils {
 
-    public static String getParameter(HttpServletRequest req, String name){
+    public static String getParameter(final HttpServletRequest req, final String name) {
 
-        ServletContext sc = req.getServletContext();
-        String v = sc.getInitParameter( name );
-        if(v == null){
-            v = sc.getInitParameter( name + ".default" );
+        final ServletContext sc = req.getServletContext();
+        String v = sc.getInitParameter(name);
+        if (v == null) {
+            v = sc.getInitParameter(name + ".default");
         }
         return v;
     }
 
-    // As getParameter but the value is a path.  For .default parameters the
+    // As getParameter but the value is a path. For .default parameters the
     // path is always relative to the web home directory, i.e. webapp, but
     // as the true file system path, e.g. /home/pi/jetty-base/minsky-base/webapp/...
     // For user defined parameters the same applies unless the first char is /
-    // in which case it is an absolute path for the host file system, e.g. /etc/passwd   
-    public static String getPath(HttpServletRequest req, String name){
+    // in which case it is an absolute path for the host file system, e.g.
+    // /etc/passwd
+    public static String getPath(final HttpServletRequest req, final String name) {
 
-        ServletContext sc = req.getServletContext();
+        final ServletContext sc = req.getServletContext();
         String p = null;
         String v = sc.getInitParameter( name );
         if( v != null){
@@ -39,8 +40,5 @@ public class Utils {
             }
         }
         return p;
-
-    }
-
-    
+    }  
 }
